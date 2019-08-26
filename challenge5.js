492d7788784bff2e3d95b900517ec59935bc70d7
Knut Lunde <kaataknut@gmail.com>
2019-08-26 12:49:03 +0200

    challenge5

---
+++ challenge5.js
@@ -0,0 +1,46 @@
+function addg(first) {
+  function more(next) {
+    if (next === undefined) {
+      return first;
+    }
+
+    first += next;
+    return more;
+  }
+
+  return first === undefined ? undefined : more;
+}
+
+function liftg(binary) {
+  return function(first) {
+    function more(next) {
+      if (next === undefined) {
+        return first;
+      }
+
+      first = binary(first, next);
+      return more;
+    }
+
+    return first === undefined ? undefined : more;
+  };
+}
+
+function arrayg(first) {
+  var array = [];
+
+  function more(next) {
+    if (next === undefined) {
+      return array;
+    }
+
+    array.push(next);
+    return more;
+  }
+
+  return more(first);
+}
+
+exports.addg = addg;
+exports.liftg = liftg;
+exports.arrayg = arrayg;
---
+++ challenge5.test.js
@@ -0,0 +1,37 @@
+var test = require('tape');
+var { add, mul, sub } = require('./challenge1.js');
+var { arrayg, addg, liftg } = require('./challenge5.js');
+
+test('addg', function(t) {
+  t.plan(5);
+
+  t.equal(addg(), undefined);
+  t.equal(addg(2)(), 2);
+  t.equal(addg(2)(7)(), 9);
+  t.equal(addg(3)(0)(4)(), 7);
+  t.equal(addg(1)(2)(4)(8)(), 15);
+});
+
+test('liftg', function(t) {
+  t.plan(7);
+
+  var liftgAdd = liftg(add);
+  t.equal(liftgAdd(), undefined);
+  t.equal(liftgAdd(1)(2)(4)(8)(), 15);
+
+  var liftgSub = liftg(sub);
+  t.equal(liftgSub(), undefined);
+  t.equal(liftgSub(1)(2)(4)(8)(), -13);
+
+  var liftgMul = liftg(mul);
+  t.equal(liftgMul(), undefined);
+  t.equal(liftgMul(1)(2)(4)(3)(), 24);
+  t.equal(liftgMul(1)(2)(4)(0)(4)(), 0);
+});
+
+test('arrayg', function(t) {
+  t.plan(3);
+  t.deepEqual(arrayg(), []);
+  t.deepEqual(arrayg(1)(), [1]);
+  t.deepEqual(arrayg(1)(2)(3)(), [1, 2, 3]);
+});
